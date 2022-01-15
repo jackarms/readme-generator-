967d986da8f2f6ca1fa997f4e5e398523eaf740b
jackarms <90922918+jackarms@users.noreply.github.com>
2022-01-15 01:57:50 -0600

    addition of fs and then index.html

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
 const readmeDataArgs = process.argv.slice(2, process.argv.length);
 
 const readMePage = (project, author) => {
     return `
-    Project: ${project}
-    Author: ${author}
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${project}</h1>
+      <h2>${author}</h2>
+    </body>
+    </html>
     `;
 };
 
 const project = readmeDataArgs[0];
 const author = readmeDataArgs[1];
 
-console.log(readMePage(project, author));
+fs.writeFile('index.html', readMePage(project, author), err => {
+    if (err) throw err;
+})
